80240575fc5046e840745e910b751b94cbe96358
Sun-D-Shine <winthureintun2000@gmail.com>
2023-04-26 17:20:01 +0630

    ready for test

--- server/routes/test.js
+++ server/routes/test.js
@@ -1,8 +1,27 @@
 import express from "express";
+import multer from "multer";
+import { Parser } from "xml2js";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+// configure the multer middleware to handle the file upload
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+// handle the POST request to /test
+router.post("/", upload.single("anpr.xml"), (req, res) => {
+  // use xml2js to parse the XML data in the request body
+  const parser = new Parser();
+  parser.parseString(req.file.buffer.toString(), (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(400).send("Error parsing XML");
+    } else {
+      // handle the parsed data as needed
+      console.log(result);
+      res.send("Success");
+    }
+  });
 });
 
 export { router as testRouter };
