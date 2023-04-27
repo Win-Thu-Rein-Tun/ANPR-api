a8d1a6fc93c5d0363cbcc2fa1e9770ba82267253
Sun-D-Shine <winthureintun2000@gmail.com>
2023-04-27 12:26:22 +0630

    okay xml request

--- server/routes/test.js
+++ server/routes/test.js
@@ -1,25 +1,22 @@
 import express from "express";
-import multer from "multer";
-import { Parser } from "xml2js";
+// import multer from "multer";
+// import { Parser } from "xml2js";
 
 const router = express.Router();
 
 // configure the multer middleware to handle the file upload
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage });
 
 // use xml2js to parse the XML data in the request body
-const parser = new Parser();
+// const parser = new Parser();
 
 // handle the POST request to /test
-router.post("/", async (req, res) => {
-  const xmlreq = req.body;
+router.post("/", async (req, res, next) => {
+  console.log("Raw XML: " + req.rawBody);
+  console.log("Parsed XML: " + JSON.stringify(req.body));
 
-  const stringData = JSON.stringify(xmlreq);
-
-  console.log(stringData);
-
-  res.status(200).json({ okay: "successfully" });
+  res.status(200).send("okay request");
 });
 
 router.get("/", async (req, res) => {
