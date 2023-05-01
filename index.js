608e73a9f9fbbc1396422097d001be4f79ba6a81
Sun-D-Shine <winthureintun2000@gmail.com>
2023-05-01 21:27:14 +0630

    test xml2js

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const express = require("express");
 const multer = require("multer");
-const fileUpload = require("express-fileupload");
+// const fileUpload = require("express-fileupload");
 const xmlparser = require("express-xml-bodyparser");
 const cors = require("cors");
 
-// const upload = multer({ dest: "uploads/", encoding: "multipart/form-data" });
+const upload = multer({ dest: "uploads/", encoding: "multipart/form-data" });
 
 const app = express();
 app.use(cors());
-app.use(xmlparser());
-app.use(fileUpload());
+// app.use(xmlparser());
+// app.use(fileUpload());
 
-app.post("/test", (req, res) => {
+app.post("/test", upload.any(), (req, res) => {
   const contentType = req.headers;
 
   console.log(contentType);
+
+  const parser = new xml2js.Parser();
+
+  parser.parseString(req.files.buffer.toString(), (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(400).send('Error parsing XML');
+    } else {
+      // handle the parsed data as needed
+      console.log(result);
+      res.send('Success');
+    }
+  });
   console.log(req.body); // anpr.xml file is in req.body
   console.log(req.files); // licensePlatePicture.jpg and detectionPicture.jpg are in req.files
   res.status(200).send("Success!");
