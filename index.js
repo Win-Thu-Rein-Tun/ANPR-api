c778496144ad073697354887b24c0bdf4cb55b22
Sun-D-Shine <winthureintun2000@gmail.com>
2023-05-01 22:01:50 +0630

    fifth test xml2js

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 const express = require("express");
 const multer = require("multer");
-// const fileUpload = require("express-fileupload");
-// const xmlparser = require("express-xml-bodyparser");
+const xmlparser = require("express-xml-bodyparser");
 const cors = require("cors");
 const xml2js = require("xml2js");
 
-const upload = multer({ dest: "uploads/", encoding: "multipart/form-data" });
+const upload = multer({ dest: "uploads/" });
 
 const app = express();
 app.use(cors());
-// app.use(xmlparser());
-// app.use(fileUpload());
+app.use(xmlparser());
 
-app.post("/test", upload.any(), (req, res) => {
+// configure multer middleware to handle file uploads
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./uploads");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+// const upload = multer({ storage: storage });
+
+app.post("/test", upload.array("file"), (req, res) => {
   const contentType = req.headers;
 
   console.log(contentType);
 
   const parser = new xml2js.Parser();
 
-  parser.parseString(req.files.buffer.toString(), (err, result) => {
+  parser.parseString(req.body, (err, result) => {
     if (err) {
       console.error(err);
       res.status(400).send("Error parsing XML");
     } else {
       // handle the parsed data as needed
       console.log(result);
+      console.log(req.files); // licensePlatePicture.jpg and detectionPicture.jpg are in req.files
       res.send("Success");
     }
   });
-
-  console.log(req.body); // anpr.xml file is in req.body
-  console.log(req.files); // licensePlatePicture.jpg and detectionPicture.jpg are in req.files
-  res.status(200).send("Success!");
 });
 
 app.listen(3000, () => {
