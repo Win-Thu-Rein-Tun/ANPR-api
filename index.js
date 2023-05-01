127cebb1a733a5398e99934849a43f88315ba396
Sun-D-Shine <winthureintun2000@gmail.com>
2023-05-01 22:13:32 +0630

    7th try for xml

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 const express = require("express");
 const multer = require("multer");
-const xmlparser = require("express-xml-bodyparser");
-const cors = require("cors");
 const xml2js = require("xml2js");
-
-const upload = multer({ dest: "uploads/" });
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(xmlparser());
 
 // configure multer middleware to handle file uploads
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "./uploads");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   },
-// });
-// const upload = multer({ storage: storage });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
 
-app.post("/test", upload.any(), (req, res) => {
-  const contentType = req.headers;
+// configure body parser middleware to handle the text/xml body
+app.use(bodyParser.text({ type: "text/xml" }));
 
-  console.log(contentType);
-  console.log(JSON.stringify(req.body));
-  console.log(JSON.stringify(req.files)); // licensePlatePicture.jpg and detectionPicture.jpg are in req.files
+// define a route to handle the request
+app.post("/test", upload.array("file"), (req, res) => {
+  // parse the XML body using xml2js
+  const parser = new xml2js.Parser();
+  parser.parseString(req.body, function (err, result) {
+    if (err) {
+      console.error(err);
+      return res.status(400).send("Error parsing XML body");
+    }
+    // handle the parsed XML data and uploaded files as needed
+    console.log(result);
+    console.log(req.files);
+    res.send("Request handled successfully");
+  });
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+// start the server
+app.listen(3000, () => console.log("Server started on port 3000"));
