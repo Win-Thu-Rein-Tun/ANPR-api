1145f53b3065e641c52bb965e42c5b09a6411d18
Sun-D-Shine <winthureintun2000@gmail.com>
2023-05-01 14:52:36 +0630

    add xml body parser and cors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const xmlparser = require("express-xml-bodyparser");
+const cors = require("cors");
+
+const upload = multer({ limits: { fileSize: Infinity }, dest: "uploads/" });
 
 const app = express();
+app.use(cors());
+app.use(xmlparser());
 
 app.post("/test", upload.any(), (req, res) => {
-  console.log(req.body); // anpr.xml file is in req.body
+  // console.log(req.body); // anpr.xml file is in req.body
   console.log(req.files); // licensePlatePicture.jpg and detectionPicture.jpg are in req.files
   res.status(200).send("Success!");
 });
