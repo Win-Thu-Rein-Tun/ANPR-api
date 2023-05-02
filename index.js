1ebcc609488c2ee7af3433dded541b1b6e486a1c
Sun-D-Shine <winthureintun2000@gmail.com>
2023-05-02 15:29:41 +0630

    18th try for xml

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const multipart = require("@szymmis/multipart");
-const xml2js = require("xml2js");
 
 const app = express();
 
@@ -11,11 +10,8 @@ app.post("/test", (req, res) => {
   console.log(req.body);
   console.log(req.files);
 
-  xml2js.parseString(req.files.anpr.xml.data, (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    // Do something with the parsed object
-  });
+  const anprData = req.files["anpr.xml"].data;
+  console.log(anprData);
 });
 
 app.listen(3000, () => {
