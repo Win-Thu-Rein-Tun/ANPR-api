85c24a5225b4fab2ff21058149c54dfd0df877cc
Sun-D-Shine <winthureintun2000@gmail.com>
2023-05-04 16:24:52 +0630

    okay fetch anpr api data

--- index.js
+++ index.js
@@ -7,17 +7,29 @@ const app = express();
 app.use(multipart());
 
 app.post("/test", (req, res) => {
-  console.log(req.headers);
-  console.log(req.body);
-  console.log(req.files);
+  // console.log(req.headers);
+  // console.log(req.body);
+  // console.log(req.files);
 
   const anprData = req.files["anpr.xml"].data;
-  console.log(anprData);
+  const anprString = anprData.toString();
+  // console.log(anprString);
 
-  xml2js.parseString(anprData, (err, result) => {
+  xml2js.parseString(anprString, (err, result) => {
     if (err) throw err;
-    console.log(result);
-    // Do something with the parsed object
+    const jsonData = JSON.stringify(result);
+    const obj = JSON.parse(jsonData);
+    // console.log(obj);
+
+    // This is Date data
+    const dateTime = obj.EventNotificationAlert.dateTime[0];
+    const humanReadableDate = new Date(dateTime).toLocaleString();
+    console.log(humanReadableDate);
+
+    // This is licensePlateNumber data
+    const anprData = obj.EventNotificationAlert.ANPR[0];
+    const licensePlate = anprData.licensePlate[0];
+    console.log(`This is realtime license number : ${licensePlate} \n`);
   });
 });
 
